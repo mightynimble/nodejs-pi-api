fccc8837316fbcec5285552f1f3de59c50a0bc6d
bosun <bosun@indeed.com>
2020-08-09 21:59:54 -0400

    connect/disconnect/status

--- routes/expressvpn.js
+++ routes/expressvpn.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const shell = require('shelljs');
 const router = express.Router();
 
-/* GET users listing. */
-router.get('/status', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/status', (req, res, next) => {
+  const stdout = shell.exec('sudo expressvpn status').stdout;
+  const connected = stdout.length > 0 && stdout.includes('Not connected');
+  res.send({ expressvpn: { connected: connected } });
+});
+
+router.post('/connect', (req, res, next) => {
+  const stdout = shell.exec('sudo expressvpn connect').stdout;
+  const succeeded = stdout.length > 0 && stdout.includes('Connected to');
+  res.send({ expressvpn: { connected: succeeded } });
+});
+
+router.post('/disconnect', (req, res, next) => {
+  const stdout = shell.exec('sudo expressvpn disconnect').stdout;
+  const succeeded = stdout.length > 0 && stdout.includes('Disconnected.');
+  res.send({ expressvpn: { connected: !succeeded } });
 });
 
 module.exports = router;
