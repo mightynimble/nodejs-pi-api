f55f668282b46d449fc2929656b044a62db9ae29
bosun <bosun@indeed.com>
2020-08-10 00:51:49 -0400

    cors

--- routes/expressvpn.js
+++ routes/expressvpn.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const shell = require('shelljs');
 const router = express.Router();
+const cors = require('cors');
 
-router.get('/status', (req, res, next) => {
+router.get('/status', cors(), (req, res, next) => {
   const stdout = shell.exec('sudo expressvpn status').stdout;
   const connected = stdout.length > 0 && !stdout.includes('Not connected');
   res.send({ expressvpn: { connected: connected } });
 });
 
-router.post('/connect', (req, res, next) => {
+router.post('/connect', cors(), (req, res, next) => {
   const stdout = shell.exec('sudo expressvpn connect').stdout;
   const succeeded = stdout.length > 0 && stdout.includes('Connected to');
   res.send({ expressvpn: { connected: succeeded } });
 });
 
-router.post('/disconnect', (req, res, next) => {
+router.post('/disconnect', cors(), (req, res, next) => {
   const stdout = shell.exec('sudo expressvpn disconnect').stdout;
   const succeeded = stdout.length > 0 && stdout.includes('Disconnected.');
   res.send({ expressvpn: { connected: !succeeded } });
